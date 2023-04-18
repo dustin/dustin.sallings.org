9035502c035e70dc426443184015c5e9f6d01b16
Dustin Sallings <dustin@spy.net>
2023-04-17 19:45:34 -1000

    Dev setup

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Blog";
+  nixConfig.bash-prompt = "\[bloggin\]$ ";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
